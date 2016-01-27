042532a68d7510546e484497e86fb97be0c3fd2c
BugDiver <vinaysh@thoughtworks.com>
2016-01-28 00:07:47 +0530

    [Vinay]:- gave functionality to find direct buses
    What changes have you made?
    
    * added some functions to get all direct buses from one location to
    * another. wrote some tests to check the functionality;
    
    Why do we need this change?
    
    * we need it to ask question from lib about all direct buses from a
    * location to another location.
    
    What can this change impact?
    
    * now lib can give the answer of first question given by jayanth.

--- test/busRoutesTest.js
+++ test/busRoutesTest.js
@@ -1,7 +1,7 @@
 var expect = require('chai').expect;
 var fs = require('fs');
 var path = require('path');
-var Route = require('../lib/busRoutes');
+var Transport = require('../lib/busRoutes');
 
 var fileName = path.join(__dirname,'../data/busData.txt')
 var bangloreData = fs.readFileSync(fileName,'UTF-8');
@@ -9,26 +9,11 @@ var bangloreData = fs.readFileSync(fileName,'UTF-8');
 var r;
 
 beforeEach(function(){
-	r = new Route();
+	r = new Transport();
 })
 
 describe('Transport',function(){
-	describe('addStation',function(){
-		it('should a station add a new station in city transport',function(){
-			r.addStation('majestic');
-			expect(r.stations).to.have.property('majestic');
-		});
-	});
-	describe('addBus',function(){
-		it('should add a bus to city transport',function(){
-			r.addBus('111');
-			expect(r.buses).to.have.property('111');
-
-		});
-	});
 	describe('addBusToStation',function(){
-
-
 		it('should add bus to given station',function(){
 			r.addBusToStation('111','majestic');
 			expect(r.stations).to.have.property('majestic').to.be.eql(['111']);
@@ -37,6 +22,30 @@ describe('Transport',function(){
 			r.addBusToStation('111','majestic');
 			expect(r.buses).to.have.property('111').to.be.eql(['majestic']);
 			
+		});
+	});
+
+	describe('initializeTransport',function(){
+		it('should initialize the transport according to given city data',function(){
+			r.initializeTransport(bangloreData);
+			expect(Object.keys(r.buses).length).to.be.equal(1462);
+			expect(Object.keys(r.stations).length).to.be.equal(988);
+		});
+	});
+
+	describe('allBusesPassingFrom',function(){
+		it('should give list of all buses passing through the given station',function(){
+			r.initializeTransport(bangloreData);
+			var buses = r.allBusesPassingFrom('UTTARAHALLI');
+			expect(buses.length).to.be.eql(17);
+		});
+	});
+
+	describe('allDirectBusesBetween',function(){
+		it('should give list all buses between given two locations',function(){
+			r.initializeTransport(bangloreData);
+			var buses = r.allDirectBusesBetween('MANIPAL HOSPITAL','CARMEL CONVENT SCHOOL')
+			expect(buses).to.be.eql(['411A']);
 		});
 	});
 })
